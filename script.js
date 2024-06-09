e1b45023465311fdc003e4333a7b92fce490b762
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-06-09 13:30:00 +0530

    Responsiveness added, new songs addes, new UI updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Initialize the variable 
 let songIndex = 0;
-let audioElement = new Audio("/Gulabi-Sadi.mp3");
+let audioElement = new Audio("./Assets/Songs/Gulabi-Sadi.mp3");
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar')
 let gif = document.getElementById('gif')
@@ -10,12 +10,16 @@ let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 //Here we have to start from tommarow
 let songs = [
-    { songName: "Gulabi-Sadi", filePath: "/Gulabi-Sadi.mp3", coverPath: "/1.jpeg" },
-    { songName: "Arambha-Hai-Prachand", filePath: "/Arambha-Hai-Prachand.mp3", coverPath: "/2.jpg" },
-    { songName: "Sam-Dam-Dandh-Bhed", filePath: "/Sam-Dam-Dandh-Bhed.mp3", coverPath: "/3.jpg" },
-    { songName: "Maula-Maula", filePath: "/Maula-Maula.mp3", coverPath: "/4.jpg" },
-    { songName: "Aale-Maratha", filePath: "/Aale-Maratha.mp3", coverPath: "/5.jpg" },
-    { songName: "Jalwa-Re-Jalwa", filePath: "/Jalwa-Re-Jalwa.mp3", coverPath: "/6.jpg" },
+    { songName: "Gulabi Sadi - Gajanan Joshi", filePath: "./Assets/Songs/Gulabi-Sadi.mp3"},
+    { songName: "Arambha Hai Prachand - Piyush Mishra", filePath: "./Assets/Songs/Arambha-Hai-Prachand.mp3"},
+    { songName: "Sam Dam Dandh Bhed - Kumar Wayadande", filePath: "./Assets/Songs/Sam-Dam-Dandh-Bhed.mp3"},
+    { songName: "Maula Maula", filePath: "./Assets/Songs/Maula-Maula.mp3"},
+    { songName: "Aale Maratha", filePath: "./Assets/Songs/Aale-Maratha.mp3"},
+    { songName: "Jalwa Re Jalwa - Kumar Wayadande", filePath: "./Assets/Songs/Jalwa-Re-Jalwa.mp3"},
+    { songName: "Milne hain mujhse aayi - Riyan Shaikh", filePath: "./Assets/Songs/_Milne Hai Mujhse Aayi Aashiqui 2_ Full Video Song _ Aditya Roy Kapur_ Shraddha Kapoor(MP3_70K).mp3"},
+    { songName: "Tu jo mila", filePath: "./Assets/Songs/_Tu Jo Mila_ Full Song with LYRICS - K.K. Pritam _ Salman Khan_ Harshaali _ Bajrangi Bhaijaan(MP3_70K).mp3"},
+    { songName: "Besabariyaan", filePath: "./Assets/Songs/BESABRIYAAN Full Video Song _ M. S. DHONI - THE UNTOLD STORY _ Sushant Singh Rajput(MP3_70K).mp3"},
+    { songName: "Bol do na zara - Riyan ashleel", filePath: "./Assets/Songs/BOL DO NA ZARA Full Video Song _ AZHAR _ Emraan Hashmi_ Nargis Fakhri _ Armaan Malik_ Amaal Mallik(MP3_70K).mp3"}
 ]
 
 //Handle play/pause click
